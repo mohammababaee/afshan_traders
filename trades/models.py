d4f91fd1ca43463f98f86ac6fae32930d67363cf
Mohammad Babaee <m.babaee@Am.charisma.local>
2024-01-22 10:26:08 +0330

    chore: update models field  convert Stock primary key to symbol for showing better view in output instead of id

--- trades/models.py
+++ trades/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Stock(models.Model):
     name = models.CharField(max_length=30)
-    symbol = models.CharField(max_length=18)
+    symbol = models.CharField(max_length=18, primary_key=True)
 
     def __str__(self):
         return f"{self.name} ({self.symbol})"
